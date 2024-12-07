d05840b949a8901278317cb4ad186797e9b8ae88
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-06 22:12:57 -0300

    feat: add in-memory repository

---
+++ app/backend/src/repositories/in-memory/in-memory-tasks.repository.ts
@@ -0,0 +1,23 @@
+import { Prisma, Task } from '@prisma/client'
+import { TasksRepository } from '../tasks.repository'
+
+import { randomUUID } from 'crypto'
+
+export class InMemoryTasksRepository implements TasksRepository {
+  public items: Task[] = []
+
+  async create(data: Prisma.TaskCreateInput): Promise<Task> {
+    const task: Task = {
+      id: randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      created_at: new Date(),
+      updated_at: new Date(),
+      completed_at: null,
+    }
+
+    this.items.push(task)
+
+    return task
+  }
+}
