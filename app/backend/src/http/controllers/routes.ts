9d622d187e35db90fd075038c5e01c9a18da1902
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-07 13:04:26 -0300

    feat: setup swagger for create task route

--- app/backend/src/http/controllers/routes.ts
+++ app/backend/src/http/controllers/routes.ts
@@ -1,6 +1,20 @@
 import { FastifyInstance } from 'fastify'
-import { helloWorldController } from './hello.controller'
+import { createTaskController } from './create-task.controller'
+import { z } from 'zod'
 
-export async function helloRoutes(app: FastifyInstance) {
-  app.get('/', helloWorldController)
+export async function taskRoutes(app: FastifyInstance) {
+  app.post('/tasks', {
+    schema: {
+      tags: ['tasks'],
+      description: 'Register a new task',
+      operationId: 'createTask',
+      body: z.object({
+        title: z.string(),
+        description: z.string().nullable()
+      }),
+      response: {
+        201: z.object({})
+      }
+    }
+  }, createTaskController)
 }
