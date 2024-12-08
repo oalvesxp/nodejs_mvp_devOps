9cc719960f31252c0dd862c245c25c8afd27d274
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-08 12:19:26 -0300

    feat: add fetch tasks controller

---
+++ app/backend/src/http/controllers/fetch-tasks.controller.ts
@@ -0,0 +1,23 @@
+import { makeFetchTasksUseCaseFactory } from '@/use-cases/factories/make-fetch-tasks.use-case.factory'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+const fetchTasksBodySchema = z.object({
+  page: z.coerce.number().min(1).default(1),
+})
+
+export async function fetchTasksController(
+  req: FastifyRequest,
+  rep: FastifyReply,
+) {
+  const { page } = fetchTasksBodySchema.parse(req.body)
+  const createTaskUseCase = makeFetchTasksUseCaseFactory()
+
+  const { tasks } = await createTaskUseCase.execute({
+    page
+  })
+
+  return rep.status(201).send({
+    tasks
+  })
+}
