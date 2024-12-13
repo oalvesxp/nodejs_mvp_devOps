74e760ce3c210b954ff441d36190f3b47d633c92
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-13 09:05:43 -0300

    iac: add terraform base config

---
+++ terraform/bastion/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region where the Network resources will be deployed, e.g., 'us-east-1' for the USA (Virgínia) region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Deployment environment name, such as 'dev', 'test', 'prod'. This categories the Network resources by their usage stage"
+  type        = string
+  default     = "dev"
+}
