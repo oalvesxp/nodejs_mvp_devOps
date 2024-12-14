e928661b0190236722e95475393f305cd3562f69
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-14 13:33:16 -0300

    iac: add terraform base config

---
+++ terraform/ecs/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  account_id                 = data.aws_caller_identity.current.account_id
+  namespaced_department_name = "${var.department_name}-${var.environment}"
+  namespaced_service_name    = "${var.service_name}-${var.environment}"
+
+  common_tags = {
+    Project    = "Node.js MVP DevOps"
+    Component  = "ECS Fargate"
+    CreatedAt  = "2024-12-14"
+    ManagedBy  = "Terraform"
+    Owner      = "Osvaldo Alves"
+    Env        = var.environment
+    Repository = "github.com/oalvesxp/nodejs_mvp_devOps"
+  }
+}
