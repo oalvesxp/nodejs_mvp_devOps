213f5d0c445ea66be5a5b19143ea5d934cbd7927
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-18 13:22:33 -0300

    fix: update indentation

--- terraform/ecr/ecr.tf
+++ terraform/ecr/ecr.tf
@@ -6,18 +6,20 @@ resource "aws_ecr_repository" "this" {
 resource "aws_ecr_lifecycle_policy" "this" {
   repository = aws_ecr_repository.this.name
   policy = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "Keep last 5 images"
-      action = {
-        type = "expire"
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        action = {
+          type = "expire"
+        }
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
       }
-      selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 5
-      }
-    }]
+    ]
   })
 }
 
