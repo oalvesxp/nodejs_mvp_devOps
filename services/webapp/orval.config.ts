ef5ddbacbb1d3c9b3c724aa791d6d2adbc964c30
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-19 11:45:48 -0300

    feat: setup swagger api schema export

---
+++ services/webapp/orval.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'orval'
+
+export default defineConfig({
+  api: {
+    input: '../api/src/swagger.json',
+    output: {
+      clean: true,
+      target: './src/http/generated/api.ts',
+      mode: 'tags-split',
+      httpClient: 'fetch',
+      client: 'fetch',
+      baseUrl: 'http://localhost:9080'
+    }
+  }
+})
