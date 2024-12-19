f95544560c75f1f481dfd3d1708f3cb11eff9262
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-19 13:52:59 -0300

    feat: setup layout responsive on home page

--- services/webapp/src/pages/Home/index.tsx
+++ services/webapp/src/pages/Home/index.tsx
@@ -6,9 +6,22 @@ function Home() {
     <div className={styles.container}>
       <main>
         <div className={styles.container__logo}>
-          <img className={styles.container__hero} src={hero} alt='Hero image' />
+          <img className={styles.hero} src={hero} alt='Hero image' />
         </div>
-        <h1 className={styles.container__title}>Sistema feito para você organizar <br /> seus estudos e tarefas</h1>
+        <h1 className={styles.container__title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+
+        <div className={styles.container__content}>
+          <section className={styles.box}>
+            <span>+ 12 posts</span>
+          </section>
+
+          <section className={styles.box}>
+            <span>+ 90 comentários</span>
+          </section>
+        </div>
       </main>
     </div>
   )
