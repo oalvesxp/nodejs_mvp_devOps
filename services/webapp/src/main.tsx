80e6b5473a34ffb68dee4eb0b55e6a1dd186803a
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-19 14:10:55 -0300

    feat: add layout config

--- services/webapp/src/main.tsx
+++ services/webapp/src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import Layout from './Layout'
+
 import './globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
   </StrictMode>
 )
