e39c12b4ec688510038c6bb37bf9c8fdb40eff03
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-19 12:30:26 -0300

    feat: add page to list task by ID

---
+++ services/webapp/src/pages/ListTaskById.tsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom'
+import '../App.css'
+
+function ListTaskById() {
+  const { slug } = useParams()
+
+  return (
+    <>
+      <h1>Tasks by ID</h1>
+      <p>This is the content of the blog post with slug {slug}.</p>
+    </>
+  )
+}
+
+export default ListTaskById
