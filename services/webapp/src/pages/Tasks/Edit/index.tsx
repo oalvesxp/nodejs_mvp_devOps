b3c1651ab610cce6459bc7e8aa951f0e2bc35d94
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-21 13:47:37 -0300

    feat: edit page

--- services/webapp/src/pages/Tasks/Edit/index.tsx
+++ services/webapp/src/pages/Tasks/Edit/index.tsx
@@ -1,137 +1,120 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-
 import styles from './Edit.module.css'
 
-import { completeTask, getTask } from '../../../hooks/api/tasks/tasks'
+import { completeTask, getTask, updateTask } from '../../../hooks/api/tasks/tasks'
 import { GetTask200Task } from '@/hooks/api/api.schemas'
+
 import { Header } from '../../../components/Header'
 import dayjs from 'dayjs'
 
 function EditTask() {
   const { id } = useParams()
+
   const [task, setTask] = useState<GetTask200Task>()
-  const [formTitle, setFormTitle] = useState('')
-  const [formDescription, setFormDescription] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
 
   async function getTaskById(id: string) {
-    try {
-      const res = await getTask(id);
-      setTask(res.data.task);
-    } catch (err) {
-      console.error('Error fetching task:', err)
-    }
+    const res = await getTask(id)
+
+    setTask(res.data.task)
+    setTitle(res.data.task.title)
+    setDescription(res.data.task.description || '')
   }
 
-  function formatDate(data: string | Date) {
-    return dayjs(data).format('DD/MM/YYYY HH:mm:ss');
+  const formatDate = (data: string | Date | undefined) => dayjs(data).format('DD/MM/YYYY HH:mm:ss')
+
+  async function HandleSubmit() {
+
+    const taskData = {
+      title,
+      description
+    }
+
+    if (id) {
+      await updateTask(id, taskData)
+    }
   }
 
   async function handleCompleteTask(id: string) {
-    try {
-      await completeTask(id)
-      getTaskById(id)
-    } catch (err) {
-      console.error("Erro ao salvar a tarefa: ", err)
-    }
+    await completeTask(id)
+    window.location.reload()
   }
 
   useEffect(() => {
     if (id) {
-      getTaskById(id);
+      getTaskById(id)
     }
-  }, [id]);
+  }, [id])
 
   useEffect(() => {
     if (task) {
-      setFormTitle(task.title || '')
-      setFormDescription(task.description || '')
-    }
-  }, [task]);
+      const isTitleUnchanged = title === task.title
+      const isDescriptionUnchanged = description === task.description
+      const isTitleEmpty = title.trim() === ''
 
-  const isDisabled =
-    formTitle === task?.title &&
-    formDescription === task?.description ||
-    formTitle.trim() === ''
+      setIsDisabled(isTitleEmpty || isTitleUnchanged && isDescriptionUnchanged)
+    }
+  }, [title, description, task])
 
   return (
     <>
       <Header />
 
       <main className={styles.container}>
-        <div className={styles.container__head}>
-          <section className={styles.container__form}>
-            <h1 className={styles.container__form__title}>Atualizar tarefa</h1>
+        <div className={styles.container__content}>
+          <section className={styles.section_title}>
+            <h1>Altualizar tarefa</h1>
+          </section>
 
-            <form>
-              <input
-                type="text"
-                name="title"
-                placeholder="Título da tarefa"
-                className={styles.container__form__input}
-                value={formTitle}
-                onChange={(e) => setFormTitle(e.target.value)}
-              />
+          <section className={styles.section__form}>
+            <form onSubmit={HandleSubmit}>
+              <input type="text" name="title" id="" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <textarea name="" id="" value={description} onChange={(e) => setDescription(e.target.value)} />
 
-              <textarea
-                name="description"
-                placeholder="Descrição..."
-                className={styles.container__form__textarea}
-                value={formDescription}
-                onChange={(e) => setFormDescription(e.target.value)}
-              />
+              <div className={styles.task__info}>
+                <div className={styles.task_info__content}>
+                  <strong>Data de Criação: </strong>
+                  <p>{formatDate(task?.created_at)}</p>
+                </div>
 
-              <input
-                type="text"
-                name="created_at"
-                className={styles.container__form__input}
-                value={task?.created_at && 'Criada em ' + formatDate(task.created_at)}
-                disabled
-              />
+                <div className={styles.task_info__content}>
+                  <strong>Última alteração: </strong>
+                  <p>{formatDate(task?.updated_at)}</p>
+                </div>
 
-              <input
-                type="text"
-                name="updated_at"
-                className={styles.container__form__input}
-                value={task?.updated_at && 'Última atualização ' + formatDate(task.updated_at)}
-                disabled
-              />
-
-              <input
-                type="text"
-                name="completed_ad"
-                className={styles.container__form__input}
-                value={task?.completed_at ? 'Concluída em ' + formatDate(task.completed_at) : 'Status: Pendente'}
-                disabled
-              />
+                <div className={styles.task_info__content}>
+                  <strong>Status da tarefa: </strong>
+                  {task?.completed_at
+                    ? <p>Completa</p>
+                    : <p>Pendente</p>
+                  }
+                </div>
+              </div>
 
               <button
-                className={`${styles.container__form__completeGreen} ${task?.completed_at ? styles.container__form__completeRed : ''}`}
                 type="button"
-                onClick={() => {
-                  if (task?.id) {
-                    handleCompleteTask(task.id);
-                  } else {
-                    console.error('Invalid task ID');
-                  }
-                }}
+                className={styles.button__complete}
+                onClick={() => handleCompleteTask(task.id)}
               >
-                {task?.completed_at ? 'Marcar tarefa como não concluída' : 'Marcar tarefa como concluída'}
+                Encerrar tarefa
               </button>
 
               <button
-                className={styles.container__form__button}
                 type="submit"
+                className={styles.button__submmit}
                 disabled={isDisabled}
               >
-                Salvar
+                Salvar alterações
               </button>
             </form>
           </section>
         </div>
       </main>
     </>
-  );
+  )
 }
 
 export default EditTask
