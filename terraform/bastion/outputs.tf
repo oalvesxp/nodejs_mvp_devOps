650fdc0db02705ba85f0527af6242c97bfa3d55e
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-23 15:04:22 -0300

    fix: Restart config

--- terraform/bastion/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "security_group_id" {
-  value = aws_security_group.this.id
-}
-
-output "bastion_host_public_ip" {
-  description = "Public IP of the Bastion Host"
-  value       = aws_instance.this.public_ip
-}
